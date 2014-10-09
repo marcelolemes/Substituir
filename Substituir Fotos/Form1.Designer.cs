7bc67776df063fa5311c1f4cc30b8cd4f4967b46
marcelolemes <lemmarcelo@gmail.com>
2014-10-09 07:57:53 -0300

    primeiro commit

---
+++ Substituir Fotos/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Substituir_Fotos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOri = new System.Windows.Forms.Button();
+            this.camImg = new System.Windows.Forms.TextBox();
+            this.camCurso = new System.Windows.Forms.TextBox();
+            this.btnDest = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.camImg2 = new System.Windows.Forms.OpenFileDialog();
+            this.camCurso2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnOri
+            // 
+            this.btnOri.Location = new System.Drawing.Point(208, 12);
+            this.btnOri.Name = "btnOri";
+            this.btnOri.Size = new System.Drawing.Size(75, 23);
+            this.btnOri.TabIndex = 0;
+            this.btnOri.Text = "Origem";
+            this.btnOri.UseVisualStyleBackColor = true;
+            this.btnOri.Click += new System.EventHandler(this.btnOri_Click);
+            // 
+            // camImg
+            // 
+            this.camImg.AllowDrop = true;
+            this.camImg.Location = new System.Drawing.Point(12, 12);
+            this.camImg.Name = "camImg";
+            this.camImg.Size = new System.Drawing.Size(164, 20);
+            this.camImg.TabIndex = 1;
+            this.camImg.TextChanged += new System.EventHandler(this.camImg_TextChanged);
+            this.camImg.DragEnter += new System.Windows.Forms.DragEventHandler(this.camImg_DragDrop);
+            this.camImg.MouseDown += new System.Windows.Forms.MouseEventHandler(this.camImg_MouseDown);
+            // 
+            // camCurso
+            // 
+            this.camCurso.AllowDrop = true;
+            this.camCurso.Location = new System.Drawing.Point(12, 53);
+            this.camCurso.Name = "camCurso";
+            this.camCurso.Size = new System.Drawing.Size(164, 20);
+            this.camCurso.TabIndex = 2;
+            this.camCurso.TextChanged += new System.EventHandler(this.camCurso_TextChanged);
+            this.camCurso.DragEnter += new System.Windows.Forms.DragEventHandler(this.camCurso_DragEnter);
+            this.camCurso.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.camCurso_MouseDoubleClick);
+            // 
+            // btnDest
+            // 
+            this.btnDest.Location = new System.Drawing.Point(208, 50);
+            this.btnDest.Name = "btnDest";
+            this.btnDest.Size = new System.Drawing.Size(75, 23);
+            this.btnDest.TabIndex = 3;
+            this.btnDest.Text = "Destino";
+            this.btnDest.UseVisualStyleBackColor = true;
+            this.btnDest.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(349, 12);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "FAÇA!";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // camImg2
+            // 
+            this.camImg2.FileName = "Abrir arquivo";
+            this.camImg2.Multiselect = true;
+            this.camImg2.FileOk += new System.ComponentModel.CancelEventHandler(this.camImg2_FileOk);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(313, 74);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(179, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Desejo jogar em todos os álbuns";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 121);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnDest);
+            this.Controls.Add(this.camCurso);
+            this.Controls.Add(this.camImg);
+            this.Controls.Add(this.btnOri);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Substituição!";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOri;
+        private System.Windows.Forms.TextBox camImg;
+        private System.Windows.Forms.TextBox camCurso;
+        private System.Windows.Forms.Button btnDest;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.OpenFileDialog camImg2;
+        private System.Windows.Forms.FolderBrowserDialog camCurso2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Substituir Fotos/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+namespace Substituir_Fotos
+{
+    public partial class Form1 : Form
+    {
+             
+        String[] Diretorio = new String[] { };
+          valores[] relat =new valores[]{ };
+      valores valor = new valores();
+        List <String> arquivos = new List <String>(); //apenas nomes que serão processados
+        List<String> arquivos2 = new List<String>(); //manipulador de arquivos encontrados
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (camCurso2.ShowDialog() == DialogResult.OK)
+            {
+                camCurso.Text = camCurso2.SelectedPath;
+            }
+            else {
+                MessageBox.Show("Digite por favor um caminho válido");
+            }
+        }
+
+        private void btnOri_Click(object sender, EventArgs e)
+        {
+            
+            try{
+camImg2.InitialDirectory = Directory.GetCurrentDirectory();
+            
+        }
+            catch
+            {camImg2.InitialDirectory = Directory.GetCurrentDirectory(); }
+
+            if (camImg2.ShowDialog() == DialogResult.OK) // confirma se foi obtido sucesso na hora de selecionar arquivos
+            {
+                camImg.Text = camImg2.FileName;
+                if (camImg2.FileNames.Length > 1)
+                {//caso haja mais de um arquivo selecionado recebe em uma LIST
+                    arquivos.AddRange(camImg2.FileNames);
+                }
+            }
+            else {
+                if (camImg.Text.Length < 5||camImg.Text.Equals("Multiplos arquivos selecionados")) {
+                    MessageBox.Show("Digite algum número de imagem por favor");
+                }
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (!checkBox1.Checked)
+            {
+                string nomeFoto = "";
+                if (arquivos.Count > 1) // ação caso mais de um arquivo seja selecionado
+                {
+                    foreach (String s in arquivos)
+                    {
+                        String[] parcial = s.Split('\\');
+                        nomeFoto = parcial[parcial.Length - 1];
+
+
+                       // MessageBox.Show("Nome foto " + nomeFoto);
+                        if (camCurso.Text.Length < 3)
+                        {
+                            MessageBox.Show("Por favor, digite o caminho de destino");
+                        }
+                        else
+                        {
+                            Diretorio = (Directory.GetDirectories(camCurso.Text, "20*"));
+                          //  MessageBox.Show(Diretorio.Length + " albuns");   COMENTADO ABAIXO
+
+                            foreach (String a in Diretorio)
+                            {
+
+                                arquivos2.Clear();
+                                arquivos2.AddRange(Directory.GetFileSystemEntries(a, nomeFoto, SearchOption.AllDirectories));
+                                if (arquivos2.Count > 0)
+                                {
+                                    foreach (String b in arquivos2)
+                                    {
+                                       // MessageBox.Show("Mover de: " + s + " para " + b); COMENTADO ABAIXO
+
+                                        try
+                                        {
+                                            File.Copy(s, b);
+                                        }
+                                        catch
+                                        {
+                                            if (File.Equals(s, b) == true) { }
+                                            else
+                                            {
+                                                File.Delete(b);
+                                                File.Copy(s, b);
+                                            }
+                                        }
+
+                                    }
+
+                                }
+                            }
+
+
+
+
+                        }
+
+                        // fim da busca de um arquivo
+                    }
+
+                }
+
+                else
+                {  // ação caso um arquivo seja selecionado
+
+                    if (camImg.Text.Length < 4) // caso nada esteja na caixa de texto de imagem, lança uma mensagem
+                    {
+                        MessageBox.Show("Digite algum número de imagem por favor");
+                    }
+                    else
+                    {
+
+                        String[] parcial = camImg.Text.Split('\\');
+                        nomeFoto = parcial[parcial.Length - 1];
+
+
+                       // MessageBox.Show("Nome foto " + nomeFoto);
+                        if (camCurso.Text.Length < 3)
+                        {
+                            MessageBox.Show("Por favor, digite o caminho de destino");
+                        }
+                        else
+                        {
+                            try // recebendo diretorios que começam com 20
+                            {
+                                Diretorio = (Directory.GetDirectories(camCurso.Text, "20*"));
+                            }
+                            catch {
+                                MessageBox.Show("Falha ao obter pasta de destino, por favor tente novamente");
+                            }
+                          //  MessageBox.Show(Diretorio.Length + " albuns");
+
+                            foreach (String a in Diretorio)
+                            {
+
+                                arquivos2.Clear();
+                                arquivos2.AddRange(Directory.GetFileSystemEntries(a, nomeFoto, SearchOption.AllDirectories)); //pesquisando arquivos com o mesmo nome
+                                if (arquivos2.Count > 0)
+                                {
+                                    foreach (String b in arquivos2)
+                                    {
+                                      //correndo os arquivos e caso o arquivo de origem e o de destino não sendo iguais, ele será copiado
+
+                                        try
+                                        {
+                                            File.Copy(camImg.Text, b);
+                                        }
+                                        catch
+                                        {
+                                            if (File.Equals(camImg.Text, b) == true) { }
+                                            else
+                                            {
+                                                File.Delete(b);
+                                                File.Copy(camImg.Text, b);
+                                            }
+                                        }
+
+                                    }
+
+                                }
+                            }
+
+
+
+
+                        }
+
+                        // fim da busca de um arquivo
+                    }
+
+
+                }
+                //MessageBox.Show("Arquivo " + camImg.Text);
+
+
+
+               
+
+
+
+
+            }
+            else
+            {// jogar para todos os álbuns caso assim marcado na checkbox
+                try
+                {
+                    Diretorio = (Directory.GetDirectories(camCurso.Text, "20*"));
+                    foreach (String a in Diretorio)
+                    {
+                        String[] parcial = camImg.Text.Split('\\');
+                        string nomeFoto = parcial[parcial.Length - 1];
+
+                        if (!File.Exists(a + "\\" + nomeFoto))
+                        {
+                            File.Copy(camImg.Text, a + "\\" + nomeFoto);
+                        }
+                        //  MessageBox.Show("de "+camImg.Text+" para"+ a + "\\" + nomeFoto);
+                    }
+                }
+                catch {
+                    
+                    MessageBox.Show("ERRO!","Falha ao substituir!");
+                }
+           
+            }
+    // fim botão 
+            MessageBox.Show("FIM!", "FIM!");
+        }
+
+        private void camImg_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void camImg_DragDrop(object sender, DragEventArgs e)
+        {// caso sejam arrastados arquivos para o caminho
+            Array a = (Array)e.Data.GetData(DataFormats.FileDrop);
+            if (a.Length > 1) {
+                foreach (String s in a) {
+                    arquivos.Add(s);
+                    camImg.Text ="Multiplos arquivos selecionados";
+                }
+            }
+            else
+            camImg.Text = a.GetValue(0).ToString();
+
+         //   MessageBox.Show("Tamanho de a "+a.Length);
+            
+        }
+
+        private void camImg_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void camImg2_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void camCurso_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void camCurso_DragEnter(object sender, DragEventArgs e)
+        {
+            Array a = (Array)e.Data.GetData(DataFormats.FileDrop);
+            camCurso.Text = a.GetValue(0).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText(TextDataFormat.Text)){
+                camCurso.Text = Clipboard.GetText(TextDataFormat.Text);
+            }
+        }
+
+        private void camCurso_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (Clipboard.ContainsText(TextDataFormat.Text))
+            {
+                camCurso.Text = Clipboard.GetText(TextDataFormat.Text);
+            }
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
