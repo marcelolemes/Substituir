7d63c9f5d2c36558ebed32bb752cb329248214f9
marcelolemes <lemmarcelo@gmail.com>
2015-05-17 14:14:56 -0300

    Começo alterações

---
+++ Substituir Fotos/valores.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substituir_Fotos
+{
+    public class valores
+    {
+        public string foto;
+        public int qtd;
+            
+            public valores()
+            {
+               
+            }
+            public valores(String foto, int qtd)
+            {
+                this.foto = foto;
+                this.qtd = qtd;
+            }
+
+            
+      
+    }
+}
