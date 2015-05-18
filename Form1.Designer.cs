9356afd42c630adaa817bccaf4168bd89f640acf
marcelolemes <lemmarcelo@gmail.com>
2015-05-18 07:49:16 -0300

    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace SubstituirFotos2
+{
+    partial class Sub
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sub));
+            this.caminhoCurso = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // caminhoCurso
+            // 
+            this.caminhoCurso.Location = new System.Drawing.Point(13, 24);
+            this.caminhoCurso.Name = "caminhoCurso";
+            this.caminhoCurso.Size = new System.Drawing.Size(259, 20);
+            this.caminhoCurso.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Caminho do contrato";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(95, 50);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Sub
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 77);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.caminhoCurso);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(1000, 500);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 115);
+            this.MinimumSize = new System.Drawing.Size(300, 115);
+            this.Name = "Sub";
+            this.Text = "Substituir";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox caminhoCurso;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SubstituirFotos2
+{
+    public partial class Sub : Form
+    {
+        List<String> arquivosPastaSubs = new List<String>(); //Arquivos à serem substituidos
+        List<String> arquivosEncontrados = new List<String>(); //Arquivos encontrados, que batem com o nome dos arquivos da pasta substituir
+        String[] albumFormandos = new String[] { };
+        String pastaSubs;
+
+        public Sub()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(caminhoCurso.Text)) // Verifcando se o caminho inserido é válido
+            {
+
+                albumFormandos = (Directory.GetDirectories(caminhoCurso.Text, "20*"));  // Listando todos os álbuns que começam com "20" e agrupando numa variável
+                pastaSubs = (Directory.GetDirectories(caminhoCurso.Text, "*Subs*").First());
+                MessageBox.Show(pastaSubs);
+
+            }
+
+            else {
+                                
+            }
+        }
+    }
+}
